67d6a1754056a44df7a75adbd19de78ffa0608ac
Alexander Gusev <alexander_gusev@Alexanders-MacBook-Pro.local>
2025-04-24 23:32:37 +0100

    update configs

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,6 +13,23 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+      },
+      buffers = { follow_current_file = { enable = true } },
+    }
+  end,
   opts = {
     filesystem = {
       window = {
