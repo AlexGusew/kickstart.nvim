c729ff18462b712ebb61f4c54caca799d5f1d2ab
Alexander Gusev <alexander_gusev@Alexanders-MacBook-Pro.local>
2025-05-18 16:42:31 +0100

    new changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,8 +1,8 @@
 vim.opt.verbose = 1 -- Log at level 1 (you can increase this value for more detailed logs)
-vim.opt.runtimepath:append '~/.local/state/nvim'
+vim.opt.runtimepath:append("~/.local/state/nvim")
 
-vim.opt.tabstop = 2 -- Number of spaces for a tab
-vim.opt.shiftwidth = 2 -- Number of spaces for autoindent
+vim.opt.tabstop = 2      -- Number of spaces for a tab
+vim.opt.shiftwidth = 2   -- Number of spaces for autoindent
 vim.opt.expandtab = true -- Use spaces instead of tabs
 
 vim.api.nvim_set_keymap('n', 'n', 'nzz', { noremap = true, silent = true })
@@ -12,55 +12,56 @@ vim.api.nvim_set_keymap('n', 'N', 'Nzz', { noremap = true, silent = true })
 --
 -- See the kickstart.nvim README for more information
 vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', { noremap = true, desc = '[T]ab [N]ew', silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tt', ':tabnew | term<CR>', { noremap = true, desc = '[T]ab [T]erminal', silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tt', ':tabnew | term<CR>',
+  { noremap = true, desc = '[T]ab [T]erminal', silent = true })
 
 function EditLineFromLazygit(file_path, line)
-  local path = vim.fn.expand '%:p'
+  local path = vim.fn.expand("%:p")
   if path == file_path then
     vim.cmd(tostring(line))
   else
-    vim.cmd('e ' .. file_path)
+    vim.cmd("e " .. file_path)
     vim.cmd(tostring(line))
   end
 end
 
 function EditFromLazygit(file_path)
-  local path = vim.fn.expand '%:p'
+  local path = vim.fn.expand("%:p")
   if path == file_path then
     return
   else
-    vim.cmd('e ' .. file_path)
+    vim.cmd("e " .. file_path)
   end
 end
 
 return {
   ---@type LazySpec
   {
-    'mikavilpas/yazi.nvim',
-    event = 'VeryLazy',
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
     dependencies = {
       -- check the installation instructions at
       -- https://github.com/folke/snacks.nvim
-      'folke/snacks.nvim',
+      "folke/snacks.nvim"
     },
     keys = {
       -- 👇 in this section, choose your own keymappings!
       {
-        '<leader>-',
-        mode = { 'n', 'v' },
-        '<cmd>Yazi<cr>',
-        desc = 'Open yazi at the current file',
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
       },
       {
         -- Open in the current working directory
-        '<leader>cw',
-        '<cmd>Yazi cwd<cr>',
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
         desc = "Open the file manager in nvim's working directory",
       },
       {
-        '<c-up>',
-        '<cmd>Yazi toggle<cr>',
-        desc = 'Resume the last yazi session',
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
       },
     },
     ---@type YaziConfig | {}
@@ -68,7 +69,7 @@ return {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = true,
       keymaps = {
-        show_help = '<f1>',
+        show_help = "<f1>",
       },
     },
     -- 👇 if you use `open_for_directories=true`, this is recommended
@@ -77,20 +78,19 @@ return {
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
     end,
-  },
-  {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
-    config = function()
-      require('typescript-tools').setup {
-        on_attach = function(client, bufnr)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end,
-      }
-    end,
-  },
+  }, {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  config = function()
+    require('typescript-tools').setup {
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+    }
+  end,
+},
   {
     'stevearc/conform.nvim',
     config = function()
@@ -110,7 +110,9 @@ return {
         formatters = {
           prettierd = {
             condition = function()
-              return vim.loop.fs_realpath '.prettierrc.js' ~= nil or vim.loop.fs_realpath '.prettierrc.mjs' ~= nil or vim.loop.fs_realpath '.prettierrc'
+              return vim.loop.fs_realpath '.prettierrc.js' ~= nil or
+                  vim.loop.fs_realpath '.prettierrc.mjs' ~= nil or
+                  vim.loop.fs_realpath '.prettierrc'
             end,
           },
         },
@@ -118,19 +120,19 @@ return {
     end,
   },
   {
-    'folke/persistence.nvim',
+    "folke/persistence.nvim",
     lazy = false,
     config = function()
-      require('persistence').setup()
+      require("persistence").setup()
 
-      vim.api.nvim_create_autocmd('VimEnter', {
+      vim.api.nvim_create_autocmd("VimEnter", {
         callback = function()
           if vim.fn.argc() == 0 then
-            require('persistence').load()
+            require("persistence").load()
             -- 🔁 Trigger filetype and syntax manually
             vim.defer_fn(function()
-              vim.cmd 'silent! filetype detect'
-              vim.cmd 'silent! syntax enable'
+              vim.cmd("silent! filetype detect")
+              vim.cmd("silent! syntax enable")
             end, 100)
           end
         end,
@@ -138,51 +140,51 @@ return {
     end,
   },
   {
-    'sindrets/diffview.nvim',
+    "sindrets/diffview.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim', -- required dependency
+      "nvim-lua/plenary.nvim", -- required dependency
     },
     config = function()
       -- optional: custom config
-      vim.keymap.set('n', '<leader>gd', ':DiffviewOpen<CR>', { desc = 'Open Git Diff View' })
-      vim.keymap.set('n', '<leader>gD', ':DiffviewClose<CR>', { desc = 'Close Git Diff View' })
+      vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Open Git Diff View" })
+      vim.keymap.set("n", "<leader>gD", ":DiffviewClose<CR>", { desc = "Close Git Diff View" })
     end,
   },
   {
-    'folke/trouble.nvim',
+    "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = 'Trouble',
+    cmd = "Trouble",
     keys = {
       {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
       },
       {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
       },
       {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
       },
       {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
       },
       {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
       },
     },
-  },
+  }
 }
